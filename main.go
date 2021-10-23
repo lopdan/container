68a57fd7a0b3bb0c8ff763ca126ca74f162dd76d
root <root@Daniel.localdomain>
2021-10-23 17:26:11 +0200

    run from child

--- main.go
+++ main.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/exec"
-"syscall"
+	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
-		case "run":
-			run()
-		default:
-			panic("")
+	case "run":
+		run(os.Args[2:]...)
+	case "child":
+		child(os.Args[2:]...)
+	default:
+		log.Fatal("Unknown command. Use run <command_name>, like `run /bin/bash` or `run echo hello`")
 	}
 }
 
-func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+func run(command ...string) {
+	log.Println("Executing", command, "from run")
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, command[0:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr {
-		Cloneflags: syscall.CLONE_NEWUTS, 
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
 }
+
+func child(command ...string) {
+	log.Println("Executing", command, "from child")
+
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+}
+
 
 func must(err error){
 	if err != nil {
